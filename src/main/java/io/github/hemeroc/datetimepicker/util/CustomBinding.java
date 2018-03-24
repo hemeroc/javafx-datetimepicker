bf70481943532a756603c7cf0b8376c03f2777c1
Dominik Moser <severus.drakone@gmail.com>
2018-03-24 21:55:16 +0100

    initial commit

---
+++ src/main/java/io/github/hemeroc/datetimepicker/util/CustomBinding.java
@@ -0,0 +1,37 @@
+package io.github.hemeroc.datetimepicker.util;
+
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WritableValue;
+
+import java.util.function.Function;
+
+public class CustomBinding {
+
+    public static <A, B> void bindBidirectional(Property<A> propertyA, Property<B> propertyB, Function<A, B> updateB, Function<B, A> updateA) {
+        addFlaggedChangeListener(propertyA, propertyB, updateB);
+        addFlaggedChangeListener(propertyB, propertyA, updateA);
+    }
+
+    public static <A, B> void bind(Property<A> propertyA, Property<B> propertyB, Function<A, B> updateB) {
+        addFlaggedChangeListener(propertyA, propertyB, updateB);
+    }
+
+    private static <X, Y> void addFlaggedChangeListener(ObservableValue<X> propertyX, WritableValue<Y> propertyY, Function<X, Y> updateY) {
+        propertyX.addListener(new ChangeListener<X>() {
+            private boolean alreadyCalled = false;
+
+            @Override
+            public void changed(ObservableValue<? extends X> observable, X oldValue, X newValue) {
+                if (alreadyCalled) return;
+                try {
+                    alreadyCalled = true;
+                    propertyY.setValue(updateY.apply(newValue));
+                } finally {
+                    alreadyCalled = false;
+                }
+            }
+        });
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module io.github.hemeroc.datetimepicker {
+    requires javafx.graphics;
+    requires javafx.controls;
+    exports io.github.hemeroc.datetimepicker;
+}
